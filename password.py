08aec4a08b46388309979d3ea0fa20ec9888f59b
big dawg gwood <101984762+redfrog3@users.noreply.github.com>
2026-02-11 18:25:58 +0000

    day 1
    Co-authored-by: TuffMango <TuffMango@users.noreply.github.com>

---
+++ password.py
@@ -0,0 +1,26 @@
+while True:
+    pass_foundation = input("Enter your password: ")
+    if len(pass_foundation)< 5:
+        print("Password must have at least 5 characters")
+    else:
+        break
+
+while True:
+    pass_rule_1 = input("Your password must include an uppercase letter: ")
+    def count_letters(word):
+        count_lower = 0
+        count_upper = 0
+        for char in pass_rule_1:
+            if char.isalpha():
+                if char.isupper():
+                    count_upper += 1
+                elif char.islower():
+                    count_lower +=1
+            return pass_rule_1
+        if count_upper <= 2:
+            print("Password must have 2 uppercase letters")
+        else:
+            break
+
+
+
