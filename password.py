cb17140cbdac262b1fd3e2f65b3d1eff65d1c4f3
big dawg gwood <101984762+redfrog3@users.noreply.github.com>
2026-02-12 17:27:07 +0000

    day 2

--- password.py
+++ password.py
@@ -1,17 +1,26 @@
 while True:
     pass_foundation = input("Enter your password: ")
-    if len(pass_foundation)< 5:
-        print("Password must have at least 5 characters")
+
+    length = len(pass_foundation)> 5
+    upper = any(char.isupper() for char in pass_foundation)
+    lower = any(char.islower() for char in pass_foundation)
+
+    if length and upper and lower:
+        print(f"Password succsesfully created:\n new password {pass_foundation}")
+    elif not length:
+        print("Password must be at least 5 characters long: ")
+    elif not upper:
+        print("Password must have at least 1 uppercase letter: ")
+    elif lower:
+        print("Password must have at least 1 lowercase letter: ")
     else:
         break
 
-
-while True:
-    pass_foundation = input("Your password must include an uppercase letter: ")
-    if any(char.isupper() for char in pass_rule_1):
-        break
-    else:
-        print("Your password must include an uppercase letter: ")
+   # pass_rule_1 = input("Your password must include an uppercase letter: ")
+   # if any(char.isupper() for char in pass_rule_1):
+      #  break
+    #else:
+        #print("Your password must include an uppercase letter: ")
     #def count_letters(word):
        # count_lower = 0
         #count_upper = 0
