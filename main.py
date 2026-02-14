a94ebb3173e6a0e3b7224ac47804ec2f16561112
big dawg gwood <101984762+redfrog3@users.noreply.github.com>
2026-02-14 03:37:45 +0000

    day 3

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-import requests   #allows to pull info from websites
-from bs4 import BeautifulSoup  #converts the html info into nice simple form
+import time
+import datetime
+def game_intro():
+    print("===THE PASSWORD GAME===")
+    time.sleep(2)
+    print("-Your password must meet all requirments-")
+    time.sleep(2)
+    print("-Work through each step wisely-")
+    time.sleep(2)
+    print("-steps will be skiped over if already completed-")
+    time.sleep(2)
+    print("-so start simple, and work with the game-")
+    time.sleep(2)
+    print("-have fun-")
+    time.sleep(2)
 
-def get_wordle_solution():
-    url = "https://www.tomsguide.com/news/what-is-todays-wordle-answer" #website with wordle answer
-    response = requests.get(url) #gets html info containing answer
-    soup = BeautifulSoup(response.text, 'html.parser') #makes html info easy to understand
-    word = soup.find("p", id="b772eb30-a3e1-4c4e-a514-78aa43892bb2")\
-        .find("strong")\
-        .text.strip()#         line 8 and 9 finds the answer, line 10 strips surounding down just to the answer
-    return word #returns the function
-
-wordle_solution = get_wordle_solution()
+game_intro()
 
 while True:
     pass_foundation = input("Enter your password: ")
-
-    length = len(pass_foundation) >= 8
+    length_0 = len(pass_foundation) >= 8
+    length_1 = len(pass_foundation) <= 14
     upper = any(char.isupper() for char in pass_foundation)
     lower = any(char.islower() for char in pass_foundation)
     number = any(char.isnumeric() for char in pass_foundation)
     special = any(not char.isalnum() for char in pass_foundation)
-    wordle_is = wordle_solution.lower() in pass_foundation.lower()
     months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+    days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
     month_is = any(month in pass_foundation.lower() for month in months)
-
-    if length and upper and lower and number and special and month_is and wordle_is:
-        print(f"\n*Password succsesfully created*\n\n-Your password: {pass_foundation}")
+    day_is = any(day in pass_foundation.lower() for day in days)
+    roman_numeral = ["M","X","I","V","M","C","L","D"]
+    roman_numeral_is = any(roman_num in pass_foundation for roman_num in roman_numeral)
+    if length_0 and length_1 and upper and lower and number and special and month_is and day_is and roman_numeral_is:
+        print(f"\n*Password successfully created*\n\n-Your password: {pass_foundation}")
         break
-    elif not length:
+    elif not length_0:
         print("\n-Password must contain at least 8 characters: \n")
+    elif not length_1:
+        print("\n-Password must contain below 14 characters: \n")
     elif not upper:
         print("\n-Password must contain at least 1 uppercase letter: \n")
     elif not lower:
@@ -39,8 +47,10 @@ while True:
         print("\n-Password must contain at least 1 special character: \n")
     elif not month_is:
         print("\n-Password must contain a month of the year: \n")
-    elif not wordle_is:
-        print("\nPassword must contain today's wordle solution: \n")
+    elif not day_is:
+        print("\n-Password must contain a week day: \n")
+    elif not roman_numeral_is:
+        print("\n-Password must contain a roman numeral: \n ")
     else:
         break
 
