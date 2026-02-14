1f59a28ab3efa98719e3c0ecc50757a0085b0e33
big dawg gwood <101984762+redfrog3@users.noreply.github.com>
2026-02-14 04:35:21 +0000

    day 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+
 import time
-import datetime
 def game_intro():
     print("===THE PASSWORD GAME===")
     time.sleep(2)
@@ -13,13 +13,14 @@ def game_intro():
     time.sleep(2)
     print("-have fun-")
     time.sleep(2)
-
+    print("*MAGIC WORD* - 🦄")
+    time.sleep(2)
 game_intro()
 
 while True:
     pass_foundation = input("Enter your password: ")
     length_0 = len(pass_foundation) >= 8
-    length_1 = len(pass_foundation) <= 14
+    length_1 = len(pass_foundation) <= 18
     upper = any(char.isupper() for char in pass_foundation)
     lower = any(char.islower() for char in pass_foundation)
     number = any(char.isnumeric() for char in pass_foundation)
@@ -30,13 +31,15 @@ while True:
     day_is = any(day in pass_foundation.lower() for day in days)
     roman_numeral = ["M","X","I","V","M","C","L","D"]
     roman_numeral_is = any(roman_num in pass_foundation for roman_num in roman_numeral)
-    if length_0 and length_1 and upper and lower and number and special and month_is and day_is and roman_numeral_is:
-        print(f"\n*Password successfully created*\n\n-Your password: {pass_foundation}")
+    magic_word = ["unicorn"]
+    magic_word_is = any(char in pass_foundation.lower() for char in magic_word)
+    if length_0 and length_1 and upper and lower and number and special and month_is and day_is and roman_numeral_is and magic_word_is:
+        print(f"\n*Password successfully created*\n\n-Your password: {pass_foundation}\n\n✅Password must contain at least 8 characters:\n✅Password must contain below 18 characters:\n✅Password must contain at least 1 uppercase letter:\n✅Password must contain at least 1 lowercase letter:\n✅Password must contain at least 1 number:\n✅Password must contain at least 1 special character:\n✅Password must contain a month of the year:\n✅Password must contain a week day:\n✅Password must contain a roman numeral:\n✅Password must include magic word:")
         break
     elif not length_0:
         print("\n-Password must contain at least 8 characters: \n")
     elif not length_1:
-        print("\n-Password must contain below 14 characters: \n")
+        print("\n-Password must contain below 18 characters: \n")
     elif not upper:
         print("\n-Password must contain at least 1 uppercase letter: \n")
     elif not lower:
@@ -50,7 +53,9 @@ while True:
     elif not day_is:
         print("\n-Password must contain a week day: \n")
     elif not roman_numeral_is:
-        print("\n-Password must contain a roman numeral: \n ")
+        print("\n-Password must contain a roman numeral: \n")
+    elif not magic_word_is:
+        print("\n-Password must include magic word: \n")
     else:
         break
 
